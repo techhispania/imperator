6743282cdf54778b8da4a7b59530b13217161293
Enrique Sanchez <esanchez.devel@gmail.com>
2025-12-10 13:47:08 +0100

    feat: execute jar from the deploy service

--- src/main/java/com/techhispania/imperator/common/utils/Constants.java
+++ src/main/java/com/techhispania/imperator/common/utils/Constants.java
@@ -10,4 +10,11 @@ public class Constants {
 	
 	public static final String HEADER_CONTENT_TYPE = "Content-Type";
 	public static final String HEADER_ACCEPT = "Accept";
+	
+	public static final int HTTP_CODE_SUCCESS = 200;
+	public static final int HTTP_CODE_REDIRECT = 302;
+	public static final int HTTP_CODE_REDIRECT_GET = 303;
+	public static final int HTTP_CODE_BAD_REQUEST = 400;
+	public static final int HTTP_CODE_NOT_FOUND = 404;
+	public static final int HTTP_CODE_INTERNAL_SERVER_ERROR = 500;
 }
--- src/main/java/com/techhispania/imperator/server/services/impl/DeployServiceImpl.java
+++ src/main/java/com/techhispania/imperator/server/services/impl/DeployServiceImpl.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.techhispania.imperator.common.exceptions.ImperatorException;
+import com.techhispania.imperator.common.utils.Constants;
 import com.techhispania.imperator.server.domain.model.Application;
 import com.techhispania.imperator.server.services.DeployService;
 
@@ -11,9 +12,45 @@ public class DeployServiceImpl implements DeployService {
 
 	private static final Logger logger = LogManager.getLogger(DeployServiceImpl.class);
 	
+	private static final String DEPLOYED_PATH = "../deployed/";
+	
 	@Override
 	public void deployApplication(Application application) throws ImperatorException {
 		logger.info("Deploying application: {}", application);
 		
+		if (!validateApplication(application)) {
+			throw new ImperatorException(Constants.HTTP_CODE_BAD_REQUEST, "Invalid Application received");
+		}
+		
+		String applicationPath = new StringBuilder(DEPLOYED_PATH).append(application.getFilename()).toString();
+		ProcessBuilder processBuilder = new ProcessBuilder("java", "-jar", applicationPath);
+		
+		processBuilder.inheritIO();
+		
+		try {
+			Process process = processBuilder.start();
+			logger.info("Process executed: {}", process.pid());
+			int exitCode = process.waitFor();
+			logger.info("Process finished: {}", exitCode);
+		} catch (Exception e) {
+			logger.error("Error executing java process", e);
+		}
+	}
+	
+	private boolean validateApplication(Application application) {
+		boolean isValid = true;
+		
+		if (application == null)
+			isValid = false;
+		else if (application.getName() == null || application.getName().length() == 0)
+			isValid = false;
+		else if (application.getDescription() == null || application.getDescription().length() == 0)
+			isValid = false;
+		else if (application.getFilename() == null || application.getFilename().length() == 0)
+			isValid = false;
+		else if (application.getType() == null)
+			isValid = false;
+		
+		return isValid;
 	}
 }
