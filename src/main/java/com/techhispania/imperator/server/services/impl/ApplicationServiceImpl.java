f7dcbcbd40d14904f4a4dac7913480e89faa7632
Enrique Sanchez <esanchez.devel@gmail.com>
2025-12-12 14:27:47 +0100

    feat: create application service to retrieve the applications

---
+++ src/main/java/com/techhispania/imperator/server/services/impl/ApplicationServiceImpl.java
@@ -0,0 +1,16 @@
+package com.techhispania.imperator.server.services.impl;
+
+import java.util.List;
+
+import com.techhispania.imperator.common.exceptions.ImperatorException;
+import com.techhispania.imperator.server.dto.ApplicationDTO;
+import com.techhispania.imperator.server.services.ApplicationService;
+
+public class ApplicationServiceImpl implements ApplicationService {
+
+	@Override
+	public List<ApplicationDTO> getAllDeployedApplications() throws ImperatorException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
