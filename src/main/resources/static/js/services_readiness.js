b71ec5312bfb285b28b7ddeb3c624babdd4d1202
Enrique Sanchez <esanchez.devel@gmail.com>
2025-12-17 16:21:15 +0100

    feat: get service status from javascript

--- src/main/resources/static/js/services_readiness.js
+++ src/main/resources/static/js/services_readiness.js
@@ -9,13 +9,42 @@ document.addEventListener("DOMContentLoaded", () => {
 		
 		services.forEach(service => {
 			
+			var id = service.id
+			var service_name = id.split("service_")[1]
 			var content = service.textContent
 			
-			console.log(`service: ${content}`)
+			console.log(`service ${service_name}: ${content}`)
+			
+			service_status = checkService(service_name)
+			
+			// TODO update the html page
 		})
-		
-		// TODO POST request to get port from the PID
-		
-		// TODO update the html page
 	}, 5000);
+	
+	async function checkService(service_name) {
+		try {
+			const response = await fetch(`http://localhost:8080/api/service/check`, {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+						"Accept": "appliation/json"
+					},
+					body: JSON.stringify({
+						name: service_name,
+					})
+				})
+				
+				
+			if (!response.ok) {
+				throw new Error(`HTTP error! Status: ${response.status}`)
+			}
+			
+			const data = await response.json()
+			console.log(`Port: ${data.port}, PID: ${data.pid}`)
+			
+			return data
+		} catch (err) {
+			console.error("Error checking service " + service_name, err)
+		}
+	}
 });
